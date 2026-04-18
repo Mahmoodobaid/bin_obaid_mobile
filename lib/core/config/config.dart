55a895d710d70b680c168e3c603d6b6c0a7f4521
Your Name <your_email@example.com>
2026-04-19 00:18:58 +0300

    استخدام مفتاح anon القديم لضمان جلب المنتجات

--- lib/core/config/config.dart
+++ lib/core/config/config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
   static const String supabaseUrl = 'https://ackxfnznrjufhppaznjd.supabase.co';
-  static const String supabaseAnonKey = 'sb_publishable_2kdwXrGwxt_svQSW5SgwXg_kos0CM9p';
+  static const String supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFja3hmbnpucmp1ZmhwcGF6bmpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyMjEyMzgsImV4cCI6MjA5MDc5NzIzOH0.Yzs_X6EI13jLeaDIkXwL6L-7pm-Zl3YXM4aB9Fwves8';
 }
